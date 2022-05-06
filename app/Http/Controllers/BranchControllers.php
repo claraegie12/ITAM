99e326efd69005af2e2f5f7436b163d3623a5285
Ahmad Hizqil <ahmad.hizqil@gmail.com>
2022-05-06 20:37:59 +0700

    removing  getbranch

--- app/Http/Controllers/BranchControllers.php
+++ app/Http/Controllers/BranchControllers.php
@@ -15,23 +15,7 @@ class BranchControllers extends Controller
      */
     public function index()
     {
-        //
-        $Branches = Branch::OrderBy('Name')->get();
-        return view('branch.index', compact('Branches'));
-    }
-
-    public function getBranches(Request $request) {
-        if ($request->ajax()) {
-            $data = Branch::latest()->get();
-            return Datatables::of($data)
-                ->addIndexColumn()
-                ->addColumn('action', function($row){
-                    $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
-                    return $actionBtn;
-                })
-                ->rawColumns(['action'])
-                ->make(true);
-        }
+        return view('branch.index');
     }
 
     /**
